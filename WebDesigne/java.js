99599636ebe6a1fc719823f2e6638a7332ee1942
Nome Funcionario <violisantana@gmail.com>
2020-08-26 12:46:00 -0300

    All Projects

---
+++ WebDesigne/java.js
@@ -0,0 +1,16 @@
+var doc = document.querySelector('header')
+
+function mudaCorFundo(){
+    if(doc.className == 'claro') {
+        doc.className = 'escuro'
+    }
+    else
+    {
+        doc.className = 'claro'
+    }
+
+}
+
+
+
+
